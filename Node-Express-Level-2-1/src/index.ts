bea2b329bd053cf3a14412cb46986341802fadf6
Oğuz Geylani Yılmaz <oguzgeylaniyilmaz@gmail.com>
2025-04-30 14:52:24 +0200

    update on data

--- Node-Express-Level-2-1/src/index.ts
+++ Node-Express-Level-2-1/src/index.ts
@@ -2,20 +2,86 @@
 import express from "express";
 import { IPerson } from "./models/IPerson";
 import { IStarship } from "./models/IStarship";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 const port = 3000;
 
 const people: IPerson[] = [
-  { id: 1, name: "Luke Skywalker" },
-  { id: 2, name: "Darth Vader" },
-  { id: 3, name: "Leia Organa" },
+  { id: 1, name: "Luke Skywalker", age: 19, height: 1.8 },
+  { id: 2, name: "Darth Vader", age: 45, height: 1.9 },
+  { id: 3, name: "Leia Organa", age: 19, height: 1.7 },
+  { id: 4, name: "Han Solo", age: 35, height: 1.8 },
+  { id: 5, name: "Chewbacca", age: 200, height: 2 },
+  { id: 6, name: "C-3PO", age: 50, height: 1.7 },
+  { id: 7, name: "R2-D2", age: 50, height: 1.1 },
+  { id: 8, name: "Obi-Wan Kenobi", age: 57, height: 1.8 },
+  { id: 9, name: "Yoda", age: 900, height: 0.66 },
+  { id: 10, name: "Padmé Amidala", age: 27, height: 1.65 },
 ];
 
 const starships: IStarship[] = [
-  { id: 1, name: "Millennium Falcon" },
-  { id: 2, name: "X-Wing" },
-  { id: 3, name: "TIE Fighter" },
+  {
+    id: 1,
+    name: "Millennium Falcon",
+    model: "YT-1300",
+    manufacturer: "Corellian Engineering Corporation",
+  },
+  {
+    id: 2,
+    name: "X-Wing",
+    model: "T-65 X-wing starfighter",
+    manufacturer: "Incom Corporation",
+  },
+  {
+    id: 3,
+    name: "TIE Fighter",
+    model: "TIE/ln space superiority starfighter",
+    manufacturer: "Sienar Fleet Systems",
+  },
+  {
+    id: 4,
+    name: "Star Destroyer",
+    model: "Imperial-class Star Destroyer",
+    manufacturer: "Kuat Drive Yards",
+  },
+  {
+    id: 5,
+    name: "Slave I",
+    model: "Firespray-31-class patrol and attack craft",
+    manufacturer: "Kuat Systems Engineering",
+  },
+  {
+    id: 6,
+    name: "A-Wing",
+    model: "RZ-1 A-wing interceptor",
+    manufacturer: "Alliance Underground Engineering",
+  },
+  {
+    id: 7,
+    name: "B-Wing",
+    model: "B-wing starfighter",
+    manufacturer: "Slayn & Korpil",
+  },
+  {
+    id: 8,
+    name: "Y-Wing",
+    model: "BTL Y-wing starfighter",
+    manufacturer: "Koensayr Manufacturing",
+  },
+  {
+    id: 9,
+    name: "Naboo N-1 Starfighter",
+    model: "N-1 starfighter",
+    manufacturer: "Theed Palace Space Vessel Engineering Corps",
+  },
+  {
+    id: 10,
+    name: "Jedi Starfighter",
+    model: "Delta-7 Aethersprite-class light interceptor",
+    manufacturer: "Theed Palace Space Vessel Engineering Corps",
+  },
 ];
 
 app.get("/people", (req, res) => {
--- Node-Express-Level-2-1/src/models/IPerson.ts
+++ Node-Express-Level-2-1/src/models/IPerson.ts
@@ -1,4 +1,6 @@
 export interface IPerson {
   id: number;
   name: string;
+  age: number;
+  height: number;
 }
